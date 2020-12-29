6058375d16c5c2c4119245ad3e26fe6fd08d6be6
shashank2123 <shashankkulal7@gmail.com>
2020-12-29 17:11:06 +0530

    check balance is added

--- Atm_app/urls.py
+++ Atm_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path(r'',views.index,name='index'),
     path(r'insert',views.insert,name='insert'),
     path(r'pin',views.pin,name='insert'),
+    path(r'check_balance',views.check_balance,name='check_balance')
 ]
--- Atm_app/views.py
+++ Atm_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import cardDetails
+from .models import cardDetails,Accounts
 from django.contrib import messages
 
 # Create your views here.
@@ -21,7 +21,7 @@ def pin(request):
     if request.method=='POST':
         num=request.session.get('num')
         pin=request.POST["pass"]
-        original_pin=cardDetails.objects.filter(numCard=num).values('pin')[0]['pin']
+        original_pin=cardDetails.objects.all().filter(numCard=num).values_list('pin',flat=True)[0]
         if pin==original_pin:
             return(render(request,'options.html'))
         else:
@@ -29,3 +29,8 @@ def pin(request):
             return(render(request,'pin.html'))
     else:
         return(render(request,'pin.html'))
+def check_balance(request):
+    num=request.session.get('num')
+    account_num=cardDetails.objects.all().filter(numCard=num).values_list('idAccount',flat=True)[0]
+    account_balance=Accounts.objects.all().filter(pk=account_num).values_list('balance',flat=True)[0]
+    return(render(request,'checkbl.html',{'account_balance':account_balance}))
